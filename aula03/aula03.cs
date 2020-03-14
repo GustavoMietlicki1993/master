069ef339a5eac4f2f0fff440677d17124536d586
Gustavo Rodrigues Mietlicki <gustavo.mietlicki@deal.br>
2020-03-13 21:55:56 -0300

    first commit

---
+++ aula03/aula03.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Aula03
+{
+    static void Main(string[] args){
+       int num1 = 10, num2 = 2;
+
+        Console.Write("O resultado da multiplicação entre "+num1+" e "+num2+" é igual a: "+num1 * num2);
+
+    }
+}
---
+++ aula04/aula04.cs
@@ -0,0 +1,13 @@
+using System;
+
+class aula4{
+
+    static int num = 10;
+
+    static void Main(){
+    
+      int num2 = 0;
+        Console.Write(num);
+    }
+
+}
